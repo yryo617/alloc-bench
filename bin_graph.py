ae0aac3c8766e0c5ad8e226064ece39f03b52a65
Jake Trevor <jacob.trevor234@gmail.com>
2024-09-02 16:58:38 +0100

    added new mark-time graph and colour scheme

--- bin_graph.py
+++ bin_graph.py
@@ -1,6 +1,9 @@
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+
+colours = ["#006eea", "#8872c6", "#ba77a2", "#e07d7e", "#ff8457"]
 
 
 def process(frame):
@@ -10,6 +13,7 @@ def process(frame):
         frame[k]["i"] = i
         frame[k]["r"] = r
         frame[k]["pct-gc"] = frame[k]["gc-time"] / frame[k]["total-time"]
+        frame[k]["pct-gc-mk"] = frame[k]["mark-time"] / frame[k]["gc-time"]
     return frame
 
 
@@ -55,7 +59,7 @@ def makeGraphs(data, outputFolder, treeDepth):
         offsets = [(i * width) - (width * (len(rs) - 1) / 2) for i in range(len(rs))]
         for r in range(len(rs)):
             offset = offsets[r]
-            ax.bar(x + offset, data[r], width, label=f"r= {rs[r]}")
+            ax.bar(x + offset, data[r], width, label=f"r= {rs[r]}", color=colours[r])
 
         # Adding labels and title
         ax.set_xlabel("Number of Iterations")
@@ -63,11 +67,82 @@ def makeGraphs(data, outputFolder, treeDepth):
         ax.set_title(title)
         ax.set_xticks(x)
         ax.set_xticklabels(Is)
+
         ax.legend()
         # Display the plot
         fig.savefig(f"{outputFolder}/{title}.pdf")
+
+    def markTimeChart():
+        hData = np.array(
+            [[hybrid[key]["pct-gc-mk"] for key in row] for row in cluster_keys]
+        ).T
+        pData = np.array(
+            [[purecap[key]["pct-gc-mk"] for key in row] for row in cluster_keys]
+        ).T
+
+        x = np.arange(len(hData[0]))
+        num_bars = len(rs)
+        width = 1 / (num_bars * 3 + 3)
+        offsets = [(i * 3 * width) - 1 / 3 for i in range(len(rs))]
+
+        fig, ax = plt.subplots()
+        for i in range(len(rs)):
+            offset = offsets[i]
+            ax.bar(
+                x + offset,
+                hData[i],
+                width,
+                color=colours[i],
+                label=f"r= {rs[i]}",
+                edgecolor="black",
+            )
+            ax.bar(
+                x + offset,
+                1 - hData[i],
+                width,
+                bottom=hData[i],
+                color="black",
+                edgecolor="black",
+            )
+
+            ax.bar(
+                x + offset + width,
+                pData[i],
+                width,
+                hatch="..",
+                color=colours[i],
+                edgecolor="black",
+            )
+            ax.bar(
+                x + offset + width,
+                1 - pData[i],
+                width,
+                bottom=pData[i],
+                color="black",
+                edgecolor="black",
+            )
+
+        ax.set_xlabel("Number of Iterations")
+        ax.set_ylabel("proportion of GC time spent marking")
+        ax.set_title("% time spent Marking")
+        ax.set_xticks(x)
+        ax.set_xticklabels(Is)
+
+        custom_objs = [Patch(facecolor=c, label="Color Patch") for c in colours] + [
+            Patch(facecolor="none", edgecolor="black"),
+            Patch(facecolor="none", edgecolor="black", hatch=".."),
+        ]
+
+        ax.legend(
+            custom_objs,
+            [f"r= {r}" for r in rs] + ["hybrid", "purecap"],
+            bbox_to_anchor=(1, 1),
+        )
+
+        fig.savefig(f"{outputFolder}/mark-time.pdf")
 
     mkChart(makeNormalisedData("total-time").T, "total-time", "Total time (h/p)")
     mkChart(makeNormalisedData("pct-gc").T, "pct-gc", "proportion of time spent on gc")
     mkChart(makeNormalisedData("L2D_CACHE_REFILL").T, "cache-refils", "")
     mkChart(makeNormalisedData("rss-kb").T, "rss-kb", "")
+    markTimeChart()
