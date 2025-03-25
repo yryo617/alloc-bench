ace263f2d71360fda1e1ea95a07d7b22d9530399
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2025-03-25 18:26:28 +0000

    fixing grav.c function def.

---
+++ barnes/grav.h
@@ -0,0 +1,9 @@
+#ifndef _GRAV_H_
+#define _GRAV_H_
+void hackgrav(bodyptr p,unsigned ProcessId);
+void gravsub(register nodeptr p, unsigned ProcessId, int level);
+void walksub(nodeptr n, real dsq, unsigned ProcessId);
+void hackwalk(proced sub, unsigned ProcessId);
+bool subdivp(register nodeptr p, real dsq, unsigned ProcessId);
+
+#endif
